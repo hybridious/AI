09b22f0124179cf5efc8719741fbc23aeb877a7c
0xRet2Plt <24486277+0xRet2Plt@users.noreply.github.com>
2017-12-10 20:21:04 -0800

    Create drv.cpp

---
+++ drv.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+
+int main(){
+double mean = 8.128;
+double variance = 0;
+std::cout << variance << std::endl;
+std::vector<double> prob = { 0.001, 0.006, 0.007, 0.008, 0.012, 0.020, 0.038, 0.099, 0.319, 0.437, 0.053 };
+
+for(std::vector<double>::size_type i = 0; i != prob.size(); i++){
+variance += std::pow((i-mean), 2) * prob[i];
+}
+std::cout << "The Variance is : " << variance << std::endl;
+std::cout << "The Standard Deviation is : " << std::sqrt(variance) << std::endl;
+}
