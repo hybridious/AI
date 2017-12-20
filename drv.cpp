edd16d7277f6c67d6ce4cff6e1d8ea185196357b
0xRet2Plt <24486277+0xRet2Plt@users.noreply.github.com>
2017-12-19 20:01:56 -0800

    Update drv.cpp

--- drv.cpp
+++ drv.cpp
@@ -6,12 +6,12 @@
 int main(){
 double mean = 8.128;
 double variance = 0;
-std::cout << variance << std::endl;
+std::cout << variance << '\n';
 std::vector<double> prob = { 0.001, 0.006, 0.007, 0.008, 0.012, 0.020, 0.038, 0.099, 0.319, 0.437, 0.053 };
 
 for(std::vector<double>::size_type i = 0; i != prob.size(); i++){
 variance += std::pow((i-mean), 2) * prob[i];
 }
-std::cout << "The Variance is : " << variance << std::endl;
-std::cout << "The Standard Deviation is : " << std::sqrt(variance) << std::endl;
+std::cout << "The Variance is : " << variance << '\n';
+std::cout << "The Standard Deviation is : " << std::sqrt(variance) << '\n';
 }
